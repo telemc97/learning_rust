b6e10e9a3e59aceddf1440d0b15a5ab0cf44d4bb
telemc97 <telemc97@gmail.com>
2023-04-02 18:10:28 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+struct Coordinates{
+    latitute: i32,
+    longtitude: i32,
+    alitute: i32,
+}
+
+impl Coordinates{
+    fn new()-> Coordinates {
+        Coordinates { latitute: 0, longtitude: 0, alitute: 0 }
+    }
+}
+
+fn main() {
+    let vector_capacity:usize = 100;
+    let mut vec = Vec::with_capacity(vector_capacity);
+    for _ in 0..vector_capacity{
+        vec.push(Coordinates::new());
+    }
+    let mut i:usize = 0;
+    let mut num:i32 = 0;
+
+    while i<100 {
+
+        vec[i].latitute = num*get_random_num();
+        vec[i].longtitude = num*get_random_num();
+        vec[i].alitute = num*get_random_num();
+
+        println!("Latitute {}, Longtitude {}, Altitude {}", vec[i].latitute, vec[i].longtitude, vec[i].alitute);
+        num+=1;
+        i+=1;
+    }
+}
+
+
+fn get_random_num()-> i32{
+    let number = rand::thread_rng().gen_range(0..101);
+    return  number;
+}
